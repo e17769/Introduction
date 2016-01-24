f7a4524246866eac3df3f8e0fa4daa22c655aa1f
e17769 <E17769@hotmail.com>
2016-01-24 14:44:31 -0500

    HTML

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   response.end("<html> <body> <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js'></script> <script src='image.js'> </script> <dir id='divDisplay' style='padding-top: 99px;'> <div id='divTitle' style='color:#F8F8FF;font-family: Georgia;line-height: 1.4;font-weight: normal;font-stretch: normal;font-variant: small-caps;font-size: 42px;'></div> <img id='keyImg' src='barcodeA.gif'></img> </div> </body> </html>");
 });
 
